210a7c3b96211a31744d570932305eea0009fb39
vunguyen <k10888833@berkeley.edu>
2020-10-21 00:45:20 -0700

    part2 completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,16 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+
+    @ratings_to_show = []
+    if !params[:ratings].nil?
+      @ratings_to_show = params[:ratings].keys()
+    end
+    # binding.pry
+    byebug
+    @movies = Movie.with_ratings(@ratings_to_show)
+
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+    
+    def self.all_ratings
+        ['G', 'PG', 'PG-13', 'R']
+    end
+
+    def self.with_ratings(rating_list)
+        if rating_list.empty?
+            Movie.all
+        else
+            Movie.where("rating IN (?)", rating_list)
+        end
+    end
 end
